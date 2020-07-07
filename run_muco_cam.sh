7eaae6982c6b028b4adb3296f8f2c9edfb14f74e
seonghyunkim1212 <seonghyunkim1212@gmail.com>
2020-07-07 15:56:47 +0900

    delete

--- run_muco_cam.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3 main.py -dataset fusion2 \
-                -network s2mpmr -coord cam \
-                -batch_size 10 -num_epochs 30
--- run_muco_relative.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3 main.py -dataset fusion2 \
-                -network s2mpmr -coord relative \
-                -batch_size 10 -num_epochs 30
